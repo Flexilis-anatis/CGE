02f942e27b8b33c926e6e8614e5e1022fba7e47f
Flexilis-anatis <Flexilisanatis@protonmail.com>
2018-06-19 09:52:21 -0600

    Added bash make script

---
+++ make.sh
@@ -0,0 +1,4 @@
+# Gets all files that end in .h and .cpp
+files="$(find . -type f | grep -v '\./\.git' | grep -v "\.gch" | grep -E '\.cpp|\.h' | sort -r | sed -n -e 'H;${x;s/\n/ /g;s/^,//;p;}')"
+
+g++ $files -lcurses -Wall -Wextra -Werror
